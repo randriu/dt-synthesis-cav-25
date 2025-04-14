3cc3fd23037a139f072b159e23ccc71b2954e71c
Filip Macak <filip.macak1999@gmail.com>
2025-04-14 13:30:15 +0200

    fixed q3

--- benchmark-q3.sh
+++ benchmark-q3.sh
@@ -1,5 +1,5 @@
 
-thread_count=2 # Ideally you should have 16GB of RAM per thread
+thread_count=1 # Ideally you should have 32GB of RAM per thread
 
 echo "generating dtPAYNT log files for Q3"
-python3 experiments-dts-cav.py --paynt-dir /opt/paynt --models-dir ./benchmarks-q3 --experiment-name paynt-cav-q3 --generate-csv --workers $thread_count --q3 --depth-max 4
+python3 experiments-dts-cav.py --paynt-dir /opt/paynt --models-dir ./benchmarks-q3 --experiment-name paynt-cav-q3 --generate-csv --workers $thread_count --q3 --depth-max 4 --restart --timeout 1200 --maxmem 32
