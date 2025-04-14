8e7cf2d3a6014cae0140b238a5808dc0bcf91818
Filip Macak <filip.macak1999@gmail.com>
2025-04-14 13:01:52 +0200

    fixed path for Q4

--- benchmark-q4.sh
+++ benchmark-q4.sh
@@ -1,5 +1,5 @@
 
 mkdir logs/paynt-cav-q4
 
-python3 paynt.py models-q4/consensus-4-2 --sketch model.prism --props model.props --add-dont-care-action --dt-reduction > logs/paynt-cav-q4/consensus.log
-python3 paynt.py models-q4/csma-3-4 --sketch model.prism --props model.props --add-dont-care-action --dt-reduction > logs/paynt-cav-q4/csma.log
+python3 paynt.py models-q4/consensus-4-2 --sketch model.prism --props model.props --add-dont-care-action --dt-reduction > results/logs/paynt-cav-q4/consensus.log
+python3 paynt.py models-q4/csma-3-4 --sketch model.prism --props model.props --add-dont-care-action --dt-reduction > results/logs/paynt-cav-q4/csma.log
